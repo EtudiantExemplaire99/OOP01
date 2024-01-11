91d3892805f1c3f72bc7f21d52a7b328e65841c5
maximeadam1 <30980379-maximeadam1@users.noreply.replit.com>
2024-01-11 15:52:41 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+//
+import java.util.HashMap;
+
+class Main {
+  public static void main(String[] args) {
+    HashMap<String, Integer> examScores = new HashMap<String, Integer>();
+    examScores.put("Math", 75);
+    examScores.put("English", 56);
+    examScores.put("Science", 98);
+    examScores.put("Geography", 69);
+
+    examScores.replace("Math",96);
+
+
+    examScores.put("Religion",99);
+
+    
+    System.out.println(examScores.toString());
+    
+  }
+}
